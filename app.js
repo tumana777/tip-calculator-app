02e6ee5f446d8050a8fa3b6bcc3aa136f8ec732d
tumana777 <tumana991@gmail.com>
2022-10-21 12:33:36 +0400

    add error message in case people are 0 and fix some bugs

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ const tipAmountEl = document.getElementById('tip-amount-result');
 const totalAmountEl = document.getElementById('total-amount-result');
 const customEl = document.getElementById('button-custom');
 const resetBtn = document.getElementById('reset-btn');
+const errorEl = document.getElementsByTagName('p');
 
 let billAmount, peopleAmount, tipAmount, totalAmount;
 
 let tipRate = 0.15;
 
 billAmountEl.addEventListener('input', calculate);
+billAmountEl.addEventListener('click', ()=>{
+    billAmountEl.value = '';
+});
 
 peopleAmountEl.addEventListener('input', calculate);
 
@@ -19,40 +23,48 @@ tipRateEl.forEach((tipRates) => {
 });
 
 customEl.addEventListener('input', customRate);
+customEl.addEventListener('click', customRate);
 
-resetBtn.addEventListener('click', reset);
+resetBtn.addEventListener('click', ()=>{
+    tipRateEl[2].click();
+    tipAmountEl.innerText = '$0.00';
+    totalAmountEl.innerText = '$0.00';
+    billAmountEl.value = '';
+    peopleAmountEl.value = '';
+    customEl.value = '';
+    resetBtn.classList.remove('reset-btn');
+});
 
 function calculate() {
     billAmount = +billAmountEl.value;
     peopleAmount = +peopleAmountEl.value;
     tipAmount = billAmount * tipRate;
     totalAmount = billAmount + tipAmount;
-    tipAmountEl.innerText = `$${tipAmount/peopleAmount}`;
-    totalAmountEl.innerText = `$${totalAmount/peopleAmount}`;
-    resetBtn.classList.add('reset-btn');
+
+    if (peopleAmount != 0) {
+        tipAmountEl.innerText = `$${tipAmount/peopleAmount}`;
+        totalAmountEl.innerText = `$${totalAmount/peopleAmount}`;
+        resetBtn.classList.add('reset-btn');
+        errorEl[0].style.display = 'none';
+        peopleAmountEl.style.border = 'none';
+    }else{
+        errorEl[0].style.display = 'inline-block';
+        peopleAmountEl.style.border = '2px solid #E17052';
+    }
 };
 
 function changeRate(click) {
     tipRateEl.forEach((tipRates) => {
         tipRates.classList.remove('active');
-        if(click.target.innerText == tipRates.innerText){
+        if (click.target.innerText == tipRates.innerText) {
             tipRates.classList.add('active');
-            tipRate = parseInt(tipRates.innerText)/100;
+            tipRate = parseInt(tipRates.innerText) / 100;
         }
     });
     calculate();
 };
 
-function customRate(){
-    tipRate = customEl.value/100;
+function customRate() {
+    tipRate = customEl.value / 100;
     calculate();
 };
-
-function reset(){
-    tipRateEl[2].click();
-    tipAmountEl.innerText = '$0.00';
-    totalAmountEl.innerText = '$0.00';
-    billAmountEl.value = '';
-    peopleAmountEl.value = '';
-    resetBtn.classList.remove('reset-btn');
-}
